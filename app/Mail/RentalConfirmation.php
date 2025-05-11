77c16db7114a1b772a0f08ab822bf3d3960977fc
M Rahman <ctgeye.news@gmail.com>
2025-05-12 01:21:44 +0600

    Mailable Modify

--- app/Mail/RentalConfirmation.php
+++ app/Mail/RentalConfirmation.php
@@ -3,51 +3,24 @@
 namespace App\Mail;
 
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
-use Illuminate\Mail\Mailables\Content;
-use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
+use App\Models\Rental;
 
 class RentalConfirmation extends Mailable
 {
     use Queueable, SerializesModels;
 
-    /**
-     * Create a new message instance.
-     */
-    public function __construct()
+    public $rental;
+
+    public function __construct(Rental $rental)
     {
-        //
+        $this->rental = $rental;
     }
 
-    /**
-     * Get the message envelope.
-     */
-    public function envelope(): Envelope
+    public function build()
     {
-        return new Envelope(
-            subject: 'Rental Confirmation',
-        );
-    }
-
-    /**
-     * Get the message content definition.
-     */
-    public function content(): Content
-    {
-        return new Content(
-            view: 'view.name',
-        );
-    }
-
-    /**
-     * Get the attachments for the message.
-     *
-     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
-     */
-    public function attachments(): array
-    {
-        return [];
+        return $this->subject('Your Car Rental Confirmation')
+                    ->view('emails.rental_confirmation');
     }
 }
