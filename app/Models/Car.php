c9f7882a0b60de119b5d71eecb1111df78c166e0
M Rahman <ctgeye.news@gmail.com>
2025-05-12 01:01:09 +0600

    Model File Modify

--- app/Models/Car.php
+++ app/Models/Car.php
@@ -2,9 +2,21 @@
 
 namespace App\Models;
 
+use App\Models\Rental;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Car extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'name', 'brand', 'model', 'year', 'car_type', 
+        'daily_rent_price', 'availability', 'image'
+    ];
+
+    public function rentals()
+    {
+        return $this->hasMany(Rental::class);
+    }
 }
--- app/Models/Rental.php
+++ app/Models/Rental.php
@@ -2,9 +2,26 @@
 
 namespace App\Models;
 
+use App\Models\Car;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Rental extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id', 'car_id', 'start_date', 'end_date', 'total_cost', 'status'
+    ];
+
+    public function car()
+    {
+        return $this->belongsTo(Car::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
