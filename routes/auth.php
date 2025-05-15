3d1d71b7470217c5774df9d0d08cfc810babbcd9
M Rahman <ctgeye.news@gmail.com>
2025-05-15 16:08:06 +0600

    Page route changes

--- routes/auth.php
+++ routes/auth.php
@@ -7,7 +7,6 @@ use App\Http\Controllers\Backend\DashboardController;
 Route::prefix('/')->middleware('auth')->group(function () {
     Route::get('dashboard',     [DashboardController::class, 'dashboardPage'])->name('dashboard');
     Route::get('logout',        [DashboardController::class, 'logout'])->name('logout');
-    
     Route::get('customer/cars', [DashboardController::class, 'carsPage'])->name('auth.cars');
     Route::get('customer/rentals', [DashboardController::class, 'rentalsPage'])->name('auth.rentals');
     Route::get('customer/customers', [DashboardController::class, 'customersPage'])->name('auth.customers');
